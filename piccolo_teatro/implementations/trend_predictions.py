addf8e284ce5a2d8c47a067ee3d0805da6ddec0c
Carlo Siniscalchi <c.siniscalchi@crif.com>
2025-04-20 17:21:32 +0200

    fix: load pickle

--- piccolo_teatro/implementations/trend_predictions.py
+++ piccolo_teatro/implementations/trend_predictions.py
@@ -9,15 +9,22 @@ from ..train.model_preparation import ModelData
 from ..use_cases.trend_prediction import TrendPrediction
 
 def get_trend_prediction(show_id, estimated_sales, SALES:pd.DataFrame, PERFORMANCES:pd.DataFrame, SEASONS:pd.DataFrame):
+    print("loading model...")
     model_path = os.path.join(os.path.dirname(__file__), "..", "models", "XGB_trend.pkl")
-    model = pickle.load(model_path)
+    with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+    print("ingesting data...")
     feat_eng = FeatureEngineering(SALES, PERFORMANCES, SEASONS)
     feat_eng.ingest_sales()
+    print("adding features...")
     show_df = feat_eng.SALES[feat_eng.SALES["show_id"]==show_id]
     show_df = feat_eng.add_features(show_df)
 
+    print("creating model data...")
     show_data = ModelData(df=show_df)
+    print("predicting...")
     performance_prediction = TrendPrediction(model=model, data=show_data)
     end_date = show_data.df["last_date"].iloc[0]
     predictions = performance_prediction.trend_prediction(last_date=end_date)
     return predictions
+
